f811d6954757944d052c64df8ad408f20aa9761b
Milos Milijasevic <milosrm20005@gmail.com>
2022-06-16 10:28:22 +0200

    App.js updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import BeforeHeader from "./components/BeforeHeader";
 import SocialMedia from "./components/SocialMedia";
+import Hero from "./components/Hero";
 
 const App = () =>{
     return(
@@ -11,6 +12,7 @@ const App = () =>{
             <BeforeHeader />
             <Navbar />
             <SocialMedia />
+            <Hero />
             
         </div>
 
