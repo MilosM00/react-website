a24aea2a90ded42c1b63dbca734725dfa9cf0160
Milos Milijasevic <milosrm20005@gmail.com>
2022-06-18 20:08:47 +0200

    App.js updated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SocialMedia from "./components/SocialMedia";
 import Hero from "./components/Hero";
 import MediaList from "./components/MediaList";
 import StandardPicture from "./components/StandardPicture";
+import About from "./components/About";
 
 const App = () =>{
     return(
@@ -17,6 +18,7 @@ const App = () =>{
             <Hero />
             <MediaList />
             <StandardPicture />
+            <About />
             
         </div>
 
