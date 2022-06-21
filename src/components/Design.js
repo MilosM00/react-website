4f54e50e0d9f12fabe37335f806722fd64beb188
Milos Milijasevic <milosrm20005@gmail.com>
2022-06-21 13:09:56 +0200

    Design.js updated

---
+++ src/components/Design.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Design.css";
+
+const Design = () =>{
+    return(
+
+        <div className="design">
+
+            <p className="design-title">STYLISH AXURE DESIGN</p>
+            <p className="design-info">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, itaque.</p>
+            <button className="design-button">Download</button>
+
+        </div>
+
+    );
+};
+
+export default Design;
