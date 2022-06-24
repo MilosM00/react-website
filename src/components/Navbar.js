b8a1cca12c3f297fd7d184d7b3381df737e40a47
Milos Milijasevic <milosrm20005@gmail.com>
2022-06-24 09:50:56 +0200

    Navbar.js updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,10 @@ import "./Navbar.css";
 
 const Navbar = () =>{
 
+    const [hamburger, setHamburger] = React.useState(true);
+
     const hamburgerHandle = () =>{
-        document.querySelector(`.nav-links`).classList.toggle(`active`);
+       setHamburger(!hamburger) 
     };
 
     return(
@@ -16,9 +18,15 @@ const Navbar = () =>{
                 <p className="logo-nav">RU<span className="logo-nav-span">BIC</span></p>
 
                 <p onClick={hamburgerHandle} className="hamburger">
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                    <span className="bar"></span>
+
+                    {hamburger === true &&
+                        <i className="fa fa-bars"></i>
+                    }
+
+                    {hamburger === false &&
+                        <i className="fa fa-times"></i>
+                    }
+
                 </p>
 
                 <ul className="nav-links">
@@ -31,6 +39,17 @@ const Navbar = () =>{
                 </ul>
                 
             </nav>
+
+               {hamburger === false &&
+                    <ul className="hamburger-links">
+                        <li><a href="https://github.com/" target="_blank" rel="noreferrer">Features</a></li>
+                        <li><a href="https://github.com/" target="_blank" rel="noreferrer">About</a></li>
+                        <li><a href="https://github.com/" target="_blank" rel="noreferrer">Pricing</a></li>
+                        <li><a href="https://github.com/" target="_blank" rel="noreferrer">Reviews</a></li>
+                        <li><a href="https://github.com/" target="_blank" rel="noreferrer">Contact</a></li>
+                        <li><a className="button-components" href="https://github.com/" target="_blank" rel="noreferrer">Components</a></li>
+                    </ul> 
+               }         
 
         </div>
 
