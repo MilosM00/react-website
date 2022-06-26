292c825a4a70f10ce9baf07e881220ad2832b204
Milos Milijasevic <milosrm20005@gmail.com>
2022-06-26 10:33:45 +0200

    Contact.js updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,25 +4,31 @@ import "./Contact.css";
 const Contact = () =>{
     return(
 
-        <div className="contact">
-            <p className="title-contact">CONTACT US</p>
-            <p className="short-info-contact">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <>
+            <section id="contact-section">
 
-            <div className="inline-contact">
+                <div className="contact">
+                    <p className="title-contact">CONTACT US</p>
+                    <p className="short-info-contact">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 
-                <div className="inputs-contact">
-                    <input className="input-contact" type="text" placeholder="Name" />
-                    <input className="input-contact-other" type="text" placeholder="Email" />
-                    <input className="input-contact-other" type="text" placeholder="Subject" />
+                    <div className="inline-contact">
+
+                        <div className="inputs-contact">
+                            <input className="input-contact" type="text" placeholder="Name" />
+                            <input className="input-contact-other" type="text" placeholder="Email" />
+                            <input className="input-contact-other" type="text" placeholder="Subject" />
+                        </div>
+
+                        <textarea className="textarea-contact" placeholder="Message"></textarea>
+
+                    </div>
+
+                    <button className="button-contact">Send Message</button>
+
                 </div>
 
-                <textarea className="textarea-contact" placeholder="Message"></textarea>
-
-            </div>
-
-            <button className="button-contact">Send Message</button>
-
-        </div>
+            </section>
+        </>
 
     );
 };
